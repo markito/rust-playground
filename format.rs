e3b8eabb31efe93a6f03570f6dcc125b709fa219
William Markito Oliveira <markito@redhat.com>
2018-09-28 22:32:36 -0500

    printing text

---
+++ format.rs
@@ -0,0 +1,10 @@
+fn main() {
+
+    println!("{} days", 31);
+
+    // optional params
+    println!("{0}, this is {1}. {1}, this is {0}", "Alice","Bob");
+
+    // named params
+    println!("{subject} {verb}", subject="the quick brown fox", verb="jumps over");
+}
